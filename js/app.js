49f29efc53965e024e5e9dba7cbe15bf3395423d
paulritzman <ritzman.paul@gmail.com>
2018-04-09 15:08:36 -0700

    Created function to calculate number of cookies sold per hour; Updated generateCustomers function; Created placeholder code within index.html for testing JS functions

--- js/app.js
+++ js/app.js
@@ -4,10 +4,18 @@ var storeFirstPike = {
   branch: '1st and Pike',
   minCustHourly: 23,
   maxCustHourly: 65,
-  avgCookiePerCust: 6.3,
+  avgCookiesPerCust: 6.3,
   operatingHours: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
-  cookiesSoldHourly: [],
+  numCookiesSold: [],
   generateCustomers: function() {
-    return Math.round(Math.random() * (this.maxCustHourly - this.minCustHourly) + this.minCustHourly);
+    return Math.random() * (this.maxCustHourly - this.minCustHourly) + this.minCustHourly;
+  },
+  cookiesSoldHourly: function() {
+    for (var i = 0; i < this.operatingHours.length; i++) {
+      this.numCookiesSold.push(Math.round(this.generateCustomers() * this.avgCookiesPerCust));
+      console.log(this.numCookiesSold[i]);
+    }
   }
 };
+
+storeFirstPike.cookiesSoldHourly();
