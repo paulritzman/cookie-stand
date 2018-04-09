205ba910f55ca564166fbda0c00f62ec9e5e542b
paulritzman <ritzman.paul@gmail.com>
2018-04-09 16:37:45 -0700

    Begun work on the SeaTac Airport object; Compeleted function for calculating cookies sold each hour

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 var operatingHours = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
+// Creates object to represent the 1st and Pike cookie stand
 var storeFirstPike = {
   minCustHourly: 23,
   maxCustHourly: 65,
@@ -17,14 +18,14 @@ var storeFirstPike = {
       console.log(operatingHours[i] + ': ' + this.hourlyCookiesSold[i]);
     }
   },
-  totalCookiesSoldDaily: function() { // Calculates number of cookies sold daily
+  totalCookiesSoldDaily: function() { // Calculates total number of cookies sold that day
     for (var i = 0; i < this.hourlyCookiesSold.length; i++) {
       this.dailyCookiesSold += this.hourlyCookiesSold[i];
       console.log((this.dailyCookiesSold - this.hourlyCookiesSold[i]) + ' + ' + this.hourlyCookiesSold[i] + ' = ' + this.dailyCookiesSold);
     }
   },
   renderFirstPike: function() { // Writes the number of cookies sold each hour to sales.html as <li> elements
-    // Executes both methods to instantiate the keys with data
+    // Executes both methods to instantiate each property
     this.cookiesSoldHourly();
     this.totalCookiesSoldDaily();
 
@@ -33,15 +34,35 @@ var storeFirstPike = {
     // Writes number of cookies sold each hour
     for (var i = 0; i < operatingHours.length; i++) {
       var liElement = document.createElement('li');
-      liElement.textContent = operatingHours[i] + ': ' + this.hourlyCookiesSold[i];
+      liElement.textContent = operatingHours[i] + ': ' + this.hourlyCookiesSold[i] + ' cookies';
       ulElement.appendChild(liElement);
     }
 
     // Writes total number of cookies sold that day
     liElement = document.createElement('li');
-    liElement.textContent = 'Total: ' + this.dailyCookiesSold;
+    liElement.textContent = 'Total: ' + this.dailyCookiesSold + ' cookies';
     ulElement.appendChild(liElement);
   }
 };
 
+// Creates object to represent the SeaTac Airport cookie stand
+var storeSeaTacAirport = {
+  minCustHourly: 3,
+  maxCustHourly: 24,
+  avgCookiesPerCust: 1.2,
+  hourlyCookiesSold: [],
+  dailyCookiesSold: 0,
+  generateCustomers: function() {
+    return Math.random() * (this.maxCustHourly - this.minCustHourly) + this.minCustHourly;
+  },
+  cookiesSoldHourly: function() {
+    for (var i = 0; i < operatingHours.length; i++) {
+      this.hourlyCookiesSold.push(Math.round(this.generateCustomers() * this.avgCookiesPerCust));
+      console.log(operatingHours[i] + ': ' + this.hourlyCookiesSold[i]);
+    }
+  }
+};
+
 storeFirstPike.renderFirstPike();
+console.log('------------------- NEXT STORE -------------------');
+storeSeaTacAirport.cookiesSoldHourly();
