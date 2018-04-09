8f1b9c31c37b92ad5787b11e2412630f2a39292f
paulritzman <ritzman.paul@gmail.com>
2018-04-09 14:51:11 -0700

    Created generateCustomers method for storeFirstPike object

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ var storeFirstPike = {
   avgCookiePerCust: 6.3,
   operatingHours: ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'],
   cookiesSoldHourly: [],
-  generateCustHourly = function {
-
+  generateCustomers: function() {
+    return Math.round(Math.random() * (this.maxCustHourly - this.minCustHourly) + this.minCustHourly);
   }
 };
