22f5154a3fd9e796a145afdae0c1fbc555efde30
paulritzman <ritzman.paul@gmail.com>
2018-04-09 17:17:19 -0700

    Completed SeaTac Airport object; Updated sales.html so that it renders the object

--- js/app.js
+++ js/app.js
@@ -52,24 +52,42 @@ var storeSeaTacAirport = {
   avgCookiesPerCust: 1.2,
   hourlyCookiesSold: [],
   dailyCookiesSold: 0,
-  generateCustomers: function() {
+  generateCustomers: function() { // Generates a random number of customers
     return Math.random() * (this.maxCustHourly - this.minCustHourly) + this.minCustHourly;
   },
-  cookiesSoldHourly: function() {
+  cookiesSoldHourly: function() { // Calculates number of cookies sold each hour - rounded to whole number
     for (var i = 0; i < operatingHours.length; i++) {
       this.hourlyCookiesSold.push(Math.round(this.generateCustomers() * this.avgCookiesPerCust));
       console.log(operatingHours[i] + ': ' + this.hourlyCookiesSold[i]);
     }
   },
-  totalCookiesSoldDaily: function() {
+  totalCookiesSoldDaily: function() { // Calculates total number of cookies sold that day
     for (var i = 0; i < this.hourlyCookiesSold.length; i++) {
       this.dailyCookiesSold += this.hourlyCookiesSold[i];
       console.log((this.dailyCookiesSold - this.hourlyCookiesSold[i]) + ' + ' + this.hourlyCookiesSold[i] + ' = ' + this.dailyCookiesSold);
     }
+  },
+  renderSeaTacAirport: function() { // Writes the number of cookies sold each hour to sales.html as <li> elements
+    // Executes both methods to instantiate each property
+    this.cookiesSoldHourly();
+    this.totalCookiesSoldDaily();
+
+    var ulElement = document.getElementById('seatac-airport');
+
+    // Writes number of cookies sold each hour
+    for (var i = 0; i < operatingHours.length; i++) {
+      var liElement = document.createElement('li');
+      liElement.textContent = operatingHours[i] + ': ' + this.hourlyCookiesSold[i] + ' cookies';
+      ulElement.appendChild(liElement);
+    }
+
+    // Writes total number of cookies sold that day
+    liElement = document.createElement('li');
+    liElement.textContent = 'Total: ' + this.dailyCookiesSold + ' cookies';
+    ulElement.appendChild(liElement);
   }
 };
 
 storeFirstPike.renderFirstPike();
 console.log('------------------- NEXT STORE -------------------');
-storeSeaTacAirport.cookiesSoldHourly();
-storeSeaTacAirport.totalCookiesSoldDaily();
+storeSeaTacAirport.renderSeaTacAirport();
